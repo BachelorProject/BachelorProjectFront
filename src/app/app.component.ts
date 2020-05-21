889fb12e662e42419e8ef5ff3aa050784ad4b1e8
Rogavactive <droga16@freeuni.edu.ge>
2020-05-21 15:44:38 +0400

    adjusted test cases

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import {MatDrawer} from '@angular/material/sidenav';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  title = 'Olympo';
   isScreenSmall = false;
 
   @ViewChild('drawer') public drawer: MatDrawer;
